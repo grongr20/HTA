f8798aa01613b2250b66bd15209efd92af888dd2
grongr20 <97873700+grongr20@users.noreply.github.com>
2022-08-10 16:05:39 -0500

    Update main.ps1

--- main.ps1
+++ main.ps1
@@ -1,4 +1,4 @@
 Add-Type -AssemblyName PresentationCore,PresentationFramework
-[string]$query = Get Amongused
-[string]$title = oof
+[string]$query = "Get Amongused"
+[string]$title = "oof"
 $Result = [System.Windows.MessageBox]::Show($query. $title)
